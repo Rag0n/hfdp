11260c2335b27423ffb22c4638ad7f7658f27217
Alexander Guschin <alexander.ragon@gmail.com>
2015-12-04 12:59:49 +0300

    Изменен паттерн Observer

--- Observer.playground/Contents.swift
+++ Observer.playground/Contents.swift
@@ -1,43 +1,54 @@
+//: Playground - noun: a place where people can play
+
 import Foundation
 
-// Субьект, который оповещает наблюдателей
-protocol Subject {
-    func registerObserver(observer:Observer)
-    func removeObserver(observer:Observer)
+protocol Subject: class {
+    func registerObserver(observer: Observer)
+    func removeObserver(observer: Observer)
     func notifyObservers()
 }
 
-// Наблюдатели реализуют метод, которые вызывается при изменение состояния субъекта
+// Все наблюдатели реализуют данный протокол
+// Реализовывать данный протокол могут только классы
+// Потому что при удалении идет сравнение по ссылке, а не значению
 protocol Observer: class {
-    func update(temperature:Double?, humidity:Double?, pressure:Double?)
+    func update(temperature temperature: Double, humidity: Double, pressure: Double)
 }
 
+// UI классы должны реализовывать данный протокол
+protocol DisplayElement {
+    func display()
+}
 
+// Класс субъекта, обладает 3 состояними(температура, влажность, давление) и уведомляет наблюдаетелей об изменениях
 class WeatherData: Subject {
-    private var observersList = [Observer]()
-    private var temperature:Double?
-    private var humidity:Double?
-    private var pressure:Double?
+    private var temperature: Double!
+    private var humidity: Double!
+    private var pressure: Double!
+    private var observers = [Observer]()
     
     func registerObserver(observer: Observer) {
-        observersList.append(observer)
+        observers.append(observer)
     }
     
     func removeObserver(observer: Observer) {
-        observersList = observersList.filter { $0 !== observer }
+        observers = observers.filter { $0 !== observer }
     }
     
     func notifyObservers() {
-        for observer in observersList {
-            observer.update(temperature, humidity: humidity, pressure: pressure)
+        for observer in observers {
+            // активная доставка
+            observer.update(temperature: temperature, humidity: humidity, pressure: pressure)
         }
     }
     
+    // вызов этой функции происходит извне при изменении данных(состояния)
     func measurementsChanged() {
         notifyObservers()
     }
     
-    func setMeasurements(temperature:Double?, humidity:Double?, pressure:Double?) {
+    // тестовый метод
+    func setMeasurements(temperature: Double, humidity: Double, pressure: Double) {
         self.temperature = temperature
         self.humidity = humidity
         self.pressure = pressure
@@ -45,60 +56,58 @@ class WeatherData: Subject {
     }
 }
 
+class CurrentConditionsDisplay: Observer, DisplayElement {
+    private var temperature: Double!
+    private var pressure: Double!
+    // требуется чтобы удалить себя из списка наблюдателей
+    private weak var weatherData: Subject!
+    
+    init(weatherData: Subject) {
+        self.weatherData = weatherData
+        self.weatherData.registerObserver(self)
+    }
+    
+    // humidity нас не интересует, но доставка активная - принимаем все параметры
+    func update(temperature temperature: Double, humidity: Double, pressure: Double) {
+        self.temperature = temperature
+        self.pressure = pressure
+        display()
+    }
+    
+    // зависимая функциональность в данном случае это
+    // представление температуры и давления в UI
+    func display() {
+        print("Current Temperature = \(temperature), current pressure = \(pressure)")
+    }
+}
 
-class CurrentConditions: Observer {
-    private var temperature:Double?
-    private var humidity:Double?
-    private var pressure:Double?
-    private var weatherData:Subject?
+
+class ForecastConditions: Observer, DisplayElement {
+    private var temperature:Double!
+    private var pressure:Double!
+    private var humidity:Double!
+    private var weatherData:Subject!
     
     init(weatherData:Subject) {
         self.weatherData = weatherData
-        self.weatherData?.registerObserver(self)
+        self.weatherData.registerObserver(self)
     }
     
-    func update(temperature: Double?, humidity: Double?, pressure: Double?) {
+    func update(temperature temperature: Double, humidity: Double, pressure: Double) {
         self.temperature = temperature
+        self.pressure = pressure
         self.humidity = humidity
-        self.pressure = pressure
         display()
     }
     
     func display() {
-        print("Current temperature is \(temperature)\nCurrent humidity is \(humidity)\nCurrent pressure is \(pressure)\n")
-    }
-}
-
-func == (lhs: CurrentConditions, rhs: CurrentConditions) -> Bool {
-    return lhs.temperature == rhs.temperature && lhs.humidity == rhs.humidity && lhs.pressure == rhs.pressure
-}
-
-
-class ForecastConditions: Observer {
-    private var temperature:Double?
-    private var pressure:Double?
-    private var weatherData:Subject?
-    
-    init(weatherData:Subject) {
-        self.weatherData = weatherData
-        self.weatherData?.registerObserver(self)
-    }
-    
-    func update(temperature: Double?, humidity: Double?, pressure: Double?) {
-        self.temperature = temperature
-        self.pressure = pressure
-        display()
-    }
-    
-    func display() {
-        print("Forecast temperature is \(temperature)\n Forecast pressure is \(pressure)\n")
+        print("Forecast temperature is \(temperature), Forecast pressure is \(pressure)\n")
     }
 }
 
 
-var spb = WeatherData()
-var cc = CurrentConditions(weatherData: spb)
-var fc = ForecastConditions(weatherData: spb)
-spb.setMeasurements(25, humidity: 60, pressure: 750)
-spb.removeObserver(cc)
-spb.setMeasurements(22, humidity: 80, pressure: 776)
+let weatherData = WeatherData()
+let currentConditionDisplay = CurrentConditionsDisplay(weatherData: weatherData)
+let forecastConditionDisplay = ForecastConditions(weatherData: weatherData)
+weatherData.setMeasurements(5, humidity: 5, pressure: 8)
+weatherData.setMeasurements(10, humidity: 11, pressure: 12)
